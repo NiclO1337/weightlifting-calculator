cfa5614c98f6f699949ce8bae66e505783ac9d7f
NiclO1337 <niclas.hugdahl@gmail.com>
2025-09-09 21:58:12 +0200

    Refactor 1RM input to accept floats

--- src/components/OneRepMaxInput.jsx
+++ src/components/OneRepMaxInput.jsx
@@ -1,8 +1,31 @@
-export default function OneRepMaxInput({ value = 70, onChange}) {
+import { useState, useEffect } from 'react';
+
+export default function OneRepMaxInput({ value, onChange }) {
+  const [inputValue, setInputValue] = useState(value);
+
+  useEffect(() => {
+    setInputValue(String(value));
+  }, [value]);
+
+  const handleChange = (e) => {
+    const raw = e.target.value.replace(',', '.');
+    setInputValue(raw);
+    const num = parseFloat(raw);
+    if (!isNaN(num)) {
+      onChange(num);
+    }
+  };
+
   return (
     <div className='input-group'>
       <label htmlFor='weight'>1RM: </label>
-      <input id='weight' name='weight' value={value} onChange={(e) => onChange(Number(e.target.value))} />
+      <input
+        id='weight'
+        type='text'
+        inputMode='decimal'
+        value={inputValue}
+        onChange={handleChange}
+      />
     </div>
   );
 }
