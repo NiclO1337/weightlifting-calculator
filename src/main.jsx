4947a767f3f36a320be054e232ac65b7d1f18dc6
NiclO1337 <niclas.hugdahl@gmail.com>
2025-09-13 18:20:35 +0200

    Prevent default install dialog by blocking 'beforeinstallprompt' event

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault(); // Block the default install dialog
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
