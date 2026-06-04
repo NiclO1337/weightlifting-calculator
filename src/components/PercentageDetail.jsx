886b626e1231f858cc2653b54ee31aec64216259
NiclO1337 <niclas.hugdahl@gmail.com>
2026-06-04 14:59:30 +0200

    refactor: add button inside li for accessibility

--- src/components/PercentageDetail.jsx
+++ src/components/PercentageDetail.jsx
@@ -14,8 +14,10 @@ export default function PercentageDetail({
     <div className='percentages-detail'>
       <ul>
         {range.map((p) => (
-          <li key={p} onClick={() => onSave(p)}>
+          <li>
+          <button key={p} onClick={() => onSave(p)}>
             {p}% - {roundToIncrement(oneRepMax * p / 100, rounding)} kg
+          </button>
           </li>
         ))}
       </ul>
